a00685dd826b7df68a0246a92b6a7cb00714ffb9
Luis Araujo <laraujo@cognyhub.com>
2025-05-07 03:58:38 -0300

    validações e descrição dos botões

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,29 +1,33 @@
-// src/pages/Dashboard.tsx
-import React from 'react';
 import { useUser } from '../contexts/UserContext';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     const navigate = useNavigate();
-    const { user, setUser } = useUser();
-
-    if (!user) {
-        return <div>Você precisa estar logado para acessar essa página.</div>;
-    }
+    const { user } = useUser();
 
     const handleLogout = () => {
-        navigate('/');  // Redireciona para a página de login
+        navigate('/');
     };
+
+    if (!user) {
+        return (
+            <p>
+                <div>Você precisa estar logado para acessar essa página.</div>
+                <br></br>
+                <button onClick={handleLogout}>Logout</button>
+            </p>
+        );
+    }
 
     return (
         <div>
             <h2>Bem-vindo, {user.nome}!</h2>
-            <div>
-                <Link to="/agendamentos">Ver Agendamentos</Link>
+            <div style={{paddingBottom: 5}}>
+                <Link to="/agendamentos">Meus Agendamentos</Link>
             </div>
-            <div>
-                <Link to="/agendar-exame">Agendar Exame</Link>
+            <div style={{paddingBottom: 5}}>
+                <Link to="/agendar-exame">Agendar Novo Exame</Link>
             </div>
             <br>
             </br>
