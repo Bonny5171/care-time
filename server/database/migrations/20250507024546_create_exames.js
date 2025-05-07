b65e74ea0dc77b386a2d0b8633cc45a5b3d08011
Luis Araujo <laraujo@cognyhub.com>
2025-05-07 00:01:27 -0300

    migrations da entidade exame

---
+++ server/database/migrations/20250507024546_create_exames.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('exames', table => {
+        table.increments('id').primary();
+        table.string('nome', 100).notNullable();
+        table.string('especialidade', 100).notNullable();
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('exames');
+};
