b3029251e64eec4d384cc46d15d741871154d80e
Luis Araujo <laraujo@cognyhub.com>
2025-05-07 00:16:37 -0300

    add end-point: /api/agendamentos

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/usuario/usuario.routes';
 import exameRoutes from './routes/exames/exame.routes';
+import agendamentoRoutes from './routes/agendamentos/agemdamento.routes';
 
 const app = express()
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/exames', exameRoutes);
+app.use('/api/agendamentos', agendamentoRoutes);
 
 export default app;
