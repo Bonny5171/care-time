fff7d8a7aa43cc267a30a2644cc8a8994be4ae88
Luis Araujo <laraujo@cognyhub.com>
2025-05-06 21:45:22 -0300

    criado rota http para usuario

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import userRoutes from './routes/usuario/usuario.routes';
 
 const app = express()
 
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 export default app;
