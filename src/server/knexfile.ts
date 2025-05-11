dacfcfc2789bf6b88ac3b4714bb7afbc9e9fe0d8
Luis <luis.city@gmail.com>
2025-05-11 15:01:25 -0300

    formatado scripts: environment dev e prod, build e deploy

--- src/server/knexfile.ts
+++ src/server/knexfile.ts
@@ -11,6 +11,20 @@ const config: { [key: string]: Knex.Config } = {
     migrations: {
       directory: './server/database/migrations'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './server/database/migrations'
+    }
   }
 };
 
